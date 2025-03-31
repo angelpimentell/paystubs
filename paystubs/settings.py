f0f0b3000ff28e28c6d020d1f5627fa43bed4119
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-03-31 18:52:38 -0400

    Add apps

--- paystubs/settings.py
+++ paystubs/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third
+    'rest_framework',
+
+    # Source
+    'payments'
 ]
 
 MIDDLEWARE = [
