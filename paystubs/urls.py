148db1f3cf3ff336a788150e58b3238b426155fe
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-03-31 18:52:53 -0400

    Add process url

--- paystubs/urls.py
+++ paystubs/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from payments.views.payment_view import PaymentView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('process/', PaymentView.as_view()),
 ]
