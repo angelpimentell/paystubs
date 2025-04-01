15988d23f61903f1fe722be4fd67c1911cebbd2e
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-03-31 20:06:04 -0400

    Add params receive for request

--- payments/views/payment_view.py
+++ payments/views/payment_view.py
@@ -1,6 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import csv
+import io
 
 class PaymentView(APIView):
-    def get(self, request, format=None):
-        return Response({'test': 'hola'})
+
+    def post(self, request, format=None):
+        country = request.GET.get('country', 'do')
+        credentials = request.GET.get('credentials', None)
+        company_name = request.GET.get('company', None)
+
+        decoded_file = io.StringIO(request.body.decode('utf-8'))
+        csv_reader = csv.DictReader(decoded_file)
+
+        data_list = []
+        for row in csv_reader:
+            data_list.append(row)
+
+        return Response({'test': 'post'})
