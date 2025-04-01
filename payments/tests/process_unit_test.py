83458c5234b79e33fdb1e5eab7a70d03658386ee
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-04-01 12:01:31 -0400

    Add unit tests

--- payments/tests/process_unit_test.py
+++ payments/tests/process_unit_test.py
@@ -1,18 +1,52 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
 
-class ProcessUnitTest(TestCase):
+class ProcessUnitTest(APITestCase):
+    def setUp(self):
+        self.url = "/process?country=do&credentials=admin+admin&company=default"
+
     def test_process_with_valid_data(self):
-        self.assertTrue(True)
+        csv_content = b"full_name,email,position,health_discount_amount,social_discount_amount,taxes_discount_amount,other_discount_amount,gross_salary,gross_payment,net_payment,period"
+        csv_content += b"\nAngel,angelpimentelcontact@gmail.com,Developer,1,2,3,4,5,6,7,2025-03-31"
+        response = self.client.post(
+            self.url,
+            data=csv_content,
+            content_type="text/csv",
+        )
+
+        self.assertEqual(response.status_code, 200)
 
     def test_process_with_invalid_data(self):
-        self.assertTrue(True)
+        csv_content = b"full_name,email,position,health_discount_amount,social_discount_amount,taxes_discount_amount,other_discount_amount,gross_salary,gross_payment,net_payment,period"
+        csv_content += b"\nAngel,angelpimentelcontact@gmail.com,Developer,AAAAA,2,EEE3,4,5,6,7,20121225-03-31"
+        response = self.client.post(
+            self.url,
+            data=csv_content,
+            content_type="text/csv",
+        )
 
-    def test_process_with_valid_credentials(self):
-        self.assertTrue(True)
+        self.assertEqual(response.status_code, 200)
 
     def test_process_with_invalid_credentials(self):
-        self.assertTrue(True)
+        self.url = "/process?country=do&credentials=wronguser+admin&company=default"
+        csv_content = b"full_name,email,position,health_discount_amount,social_discount_amount,taxes_discount_amount,other_discount_amount,gross_salary,gross_payment,net_payment,period"
+        csv_content += b"\nAngel,angelpimentelcontact@gmail.com,Developer,1,2,3,4,5,6,7,2025-03-31"
+        response = self.client.post(
+            self.url,
+            data=csv_content,
+            content_type="text/csv",
+        )
 
-    def test_pdf_is_rendered(self):
-        self.assertTrue(True)
+        self.assertEqual(response.status_code, 401)
+
+    def test_process_with_invalid_country_and_company(self):
+        self.url = "/process?country=wrongcontry&credentials=admin+admin&company=wrongdefault"
+        csv_content = b"full_name,email,position,health_discount_amount,social_discount_amount,taxes_discount_amount,other_discount_amount,gross_salary,gross_payment,net_payment,period"
+        csv_content += b"\nAngel,angelpimentelcontact@gmail.com,Developer,1,2,3,4,5,6,7,2025-03-31"
+        response = self.client.post(
+            self.url,
+            data=csv_content,
+            content_type="text/csv",
+        )
+
+        self.assertEqual(response.status_code, 200)
