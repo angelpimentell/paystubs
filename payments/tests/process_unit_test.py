4f1ef7bdfd3935fa464528f30d39798d804eeb45
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-04-01 11:41:46 -0400

    Add process unit test class

---
+++ payments/tests/process_unit_test.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+
+class ProcessUnitTest(TestCase):
+    def test_process_with_valid_data(self):
+        self.assertTrue(True)
+
+    def test_process_with_invalid_data(self):
+        self.assertTrue(True)
+
+    def test_process_with_valid_credentials(self):
+        self.assertTrue(True)
+
+    def test_process_with_invalid_credentials(self):
+        self.assertTrue(True)
+
+    def test_pdf_is_rendered(self):
+        self.assertTrue(True)
