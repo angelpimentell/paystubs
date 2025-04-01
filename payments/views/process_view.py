32b8e59b4b5d2d9691432050930c3eb80ce4c2a3
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-04-01 12:03:28 -0400

    Add exception for process view

--- payments/views/process_view.py
+++ payments/views/process_view.py
@@ -36,48 +36,63 @@ class ProcessView(APIView):
         emails_sends = []
 
         for row in csv_reader:
-            gross_salary = float(row['gross_salary'])
-            gross_payment = float(row['gross_payment'])
-            net_payment = float(row['net_payment'])
+            try:
+                gross_salary = float(row['gross_salary'])
+                gross_payment = float(row['gross_payment'])
+                net_payment = float(row['net_payment'])
 
-            social_discount_amount = float(row['social_discount_amount'])
-            health_discount_amount = float(row['health_discount_amount'])
-            taxes_discount_amount = float(row['taxes_discount_amount'])
-            other_discount_amount = float(row['other_discount_amount'])
+                social_discount_amount = float(row['social_discount_amount'])
+                health_discount_amount = float(row['health_discount_amount'])
+                taxes_discount_amount = float(row['taxes_discount_amount'])
+                other_discount_amount = float(row['other_discount_amount'])
 
-            total_discounts = (
-                    social_discount_amount + health_discount_amount +
-                    taxes_discount_amount + other_discount_amount
-            )
+                total_discounts = (
+                        social_discount_amount + health_discount_amount +
+                        taxes_discount_amount + other_discount_amount
+                )
 
-            html_string = render(
-                request,
-                template_name,
-                {
-                    "period": row['period'],
-                    "comprobante_de_pago": row.get('comprobante_de_pago', 'N/A'),
-                    "title": row.get('title', ''),
-                    "full_name": row['full_name'],
+                html_string = render(
+                    request,
+                    template_name,
+                    {
+                        "period": row['period'],
+                        "comprobante_de_pago": row.get('comprobante_de_pago', 'N/A'),
+                        "title": row.get('title', ''),
+                        "full_name": row['full_name'],
 
-                    "gross_salary": "{:.2f}".format(gross_salary),
-                    "gross_payment": "{:.2f}".format(gross_payment),
-                    "net_payment": "{:.2f}".format(net_payment),
+                        "gross_salary": "{:.2f}".format(gross_salary),
+                        "gross_payment": "{:.2f}".format(gross_payment),
+                        "net_payment": "{:.2f}".format(net_payment),
 
-                    "social_discount_amount": "{:.2f}".format(social_discount_amount),
-                    "health_discount_amount": "{:.2f}".format(health_discount_amount),
-                    "taxes_discount_amount": "{:.2f}".format(taxes_discount_amount),
-                    "other_discount_amount": "{:.2f}".format(other_discount_amount),
-                    "total_discounts": "{:.2f}".format(total_discounts),
-                }
-            ).content.decode("utf-8")
+                        "social_discount_amount": "{:.2f}".format(social_discount_amount),
+                        "health_discount_amount": "{:.2f}".format(health_discount_amount),
+                        "taxes_discount_amount": "{:.2f}".format(taxes_discount_amount),
+                        "other_discount_amount": "{:.2f}".format(other_discount_amount),
+                        "total_discounts": "{:.2f}".format(total_discounts),
+                    }
+                ).content.decode("utf-8")
 
-            pdfkit.from_string(html_string, row['email'].replace(".", "_") + ".pdf")
+                pdfkit.from_string(html_string, row['email'].replace(".", "_") + ".pdf")
 
-            # ACTION: Send email
+                # ACTION: Send email
 
-            emails_sends.append({
-                'email': row['email'],
-                'datetime': datetime.now()
-            })
+                emails_sends.append({
+                    'email': row['email'],
+                    'datetime': datetime.now()
+                })
+
+            except KeyError:
+                emails_sends.append({
+                    'email': row['email'],
+                    'datetime': None,
+                    'message': 'Wrong headers from CSV.'
+                })
+
+            except ValueError:
+                emails_sends.append({
+                    'email': row['email'],
+                    'datetime': None,
+                    'message': 'Wrong values from CSV.'
+                })
 
         return Response(emails_sends)
